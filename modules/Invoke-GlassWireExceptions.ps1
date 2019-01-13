482044da96b2528b3cdaa1b5bfa253aaac22f537
SecureMode <43304873+securemode@users.noreply.github.com>
2019-01-13 12:51:15 -0500

    Add files via upload

--- modules/Invoke-GlassWireExceptions.ps1
+++ modules/Invoke-GlassWireExceptions.ps1
@@ -1,24 +1,63 @@
 function Invoke-GlasswireExceptions {
 <# 
+
 .SYNOPSIS
-Dumps GlassWire Exceptions for allowed outbound internet connectivity.
+	Dumps any program exceptions configured in GlassWire Endpoint Protection software.
 
-.EXAMPLE
-PS> . .\Invoke-GlasswireExceptions.ps1
+.PARAMETER Help
+	Shows Detailed Help.
 
-Author: (@0rbz_)
+.PARAMETER List
+	Shows Brief Command Help.
 
-# https://github.com/securemode/RTK/blob/master/PowerShell/Get-GlasswireExceptions.ps1
+.PARAMETER ListenerIp
+	The Listener IP Address.
+
+.PARAMETER ListenerPort
+	The Listener Port.
+
+.EXAMPLE 
+	PS> Invoke-Connect -ListenerIp 192.168.1.1 -ListenerPort 443
+	
+.NOTES
+	Author: Fabrizio Siciliano (@0rbz_)
+
 #>
+[CmdletBinding()]
+param(
+	[Parameter(Position=1)]
+	[Switch]$Help,
+	[Switch]$List
+)
 
-	Write "`n"
-	Write "GlassWire Exceptions List"
-	Write "-------------------------"
-	$RegKey = (Get-ItemProperty 'HKLM:\SYSTEM\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules')
-	$RegKey.PSObject.Properties | ForEach-Object {
-	  If($_.Value -like '*Active=TRUE*' -and $_.Value -like '*Allow*' -and $_.Value -like '*Dir=Out|App=*'){
-		Write-Output $_.Value | ForEach-Object {$_.split("|")} | Select-String -pattern "^App"
-	  }
+	if ($Help -or $List) {
+		Write @"
+		
+ ### Invoke-GlasswireExceptions Help ###
+ ---------------------------------------
+ Available Invoke-GlasswireExceptions Commands:
+ ----------------------------------------------
+ |--------------------------------------------------------------------------------|
+ | Invoke-GlasswireExceptions                                                     |
+ |--------------------------------------------------------------------------------|
+
+   [*] Description: Dumps any program exceptions configured in GlassWire Endpoint 
+       Protection software.
+
+   [*] Usage: Invoke-GlasswireExceptions
+
+ \--------------------------------------------------------------------------------/
+
+"@
 	}
-	Write "`n"
+	else {
+		Write "`nGlassWire Exceptions List"
+		Write "-------------------------`n"
+		$RegKey = (Get-ItemProperty 'HKLM:\SYSTEM\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules')
+		$RegKey.PSObject.Properties | ForEach-Object {
+		  If($_.Value -like '*Active=TRUE*' -and $_.Value -like '*Allow*' -and $_.Value -like '*Dir=Out|App=*'){
+			Write-Output $_.Value | ForEach-Object {$_.split("|")} | Select-String -pattern "^App"
+		  }
+		}
+	}
 }
